9dfa587ad48628adec40fc9b552aba28831c70b1
Bas <Bas>
2023-05-23 15:18:04 +0200

    add mskmodelling dependencises to stats_Kira

--- stats_Kira.m
+++ stats_Kira.m
@@ -235,4 +235,102 @@ function out = calc_max(time_curve)
 
 out = max(movmean(time_curve,5));
 
+% --------------------------------------------------------------------------------------------------------------- %
+function folders = getfolders(directory, contianing_string, IgnoreCase)
+
+folders = dir (directory);
+try 
+    folders(1:2) =[];                                           % delete "../" and "./"
+catch 
+    return
+end
+folders = folders([folders.isdir]);                         % select only rows that contain folders 
+
+if nargin > 1
+    if IgnoreCase == 0
+        folders = folders(contains({folders.name},contianing_string));
+    else
+        folders = folders(contains({folders.name},contianing_string,"IgnoreCase",true));
+    end
+end
+
+
+% --------------------------------------------------------------------------------------------------------------- %
+function [rsquared,pvalue, p1,rlo,rup] = plotCorr (x,y,n,Alpha,Color, MakerSize)
+% %% Description - Basilio Goncalves (2020)
+% https://www.researchgate.net/profile/Basilio_Goncalves
+%
+% create a scatter plot and a line of best fit with a shaded area as CI 
+%
+%INPUT  
+%   x = x axis values
+%   y = y axis values
+%   n = polynomial degree (Default = 1)
+%   Alpha = alpha level betwen 0 and 1(Default = 0.05)
+%   Color = color of the plots (Default = black)
+%   MakerSize = size of the individual markers (Default = 10)
+%-------------------------------------------------------------------------
+%OUTPUT
+%   rsquared = cell matrix maximum Force value
+%   pvalue = cell matrix maximum Force value
+%   p1 = handle of the plot 
+
+if  nargin < 3 || isempty(n)
+    n= length(x);
+end
+if nargin < 4 || isempty(Alpha)
+    Alpha= 0.05;
+end
+
+if  nargin < 5 || isempty(Color)
+    Color= 'k';
+end
+
+if  nargin < 6 || isempty(MarkerSize)
+    MakerSize= 10;
+end
+
+% delete nan
+IDXnan = any(isnan([x y]),2); 
+x(IDXnan)=[];
+y(IDXnan)=[];
+
+
+% calculate polynomial and line that best fit 
+[p,S,mu] = polyfit(x,y,n); 
+[y_fit,delta] = polyval(p,x,S,mu);
+
+% calculate rsquared and p-value
+[c, pvalue,rlo,rup] = corrcoef(x,y);
+rsquared = c(1,2)^2;
+pvalue = pvalue(1,2);
+rlo = rlo(1,2);
+rup = rup(1,2);
+
+% calculate t-statistic 
+t = tinv(1-Alpha/2,S.df);
+ 
+% calculate confidence interval
+uB = y_fit+ t*delta;
+lB = y_fit- t*delta;
+
+
+p = sortrows([x y y_fit uB lB],1);                                  %plot data
+x= p(:,1)';
+y= p(:,2)';
+y_fit= p(:,3)';
+uB= p(:,4)';
+lB= p(:,5)';
+
+hold on
+p1 = plot(x,y,'o','MarkerSize',MakerSize,'MarkerFaceColor',Color,'MarkerEdgeColor','none');
+
+p1(end+1) = plot(x,y_fit,'Color',Color,'LineWidth',2);              % plot mean trendline
+
+X=[x,fliplr(x)];                                                    % create continuous x value row vector for plotting
+Y=[lB fliplr(uB)];                                                  % create y values for out and then back (Remove NaN)
+f1 = fill(X,Y,'r');
+alpha 0.2                                                           % transparency 
+set(f1,'FaceColor', Color,'EdgeColor','none')
+
 
